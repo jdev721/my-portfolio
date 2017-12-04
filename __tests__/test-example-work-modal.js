c446658c05b4bf13064fc9ebbf26de52dc423e83
Jorge Rodriguez <captainjorge@gmail.com>
2017-12-03 23:42:41 -0500

    Fixed Enzyme Tests and added remaining fixes to Modals

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 import ExampleWorkModal from '../js/example-work-modal';
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExampleWork from './example-work';
+import ExampleWork from './example-work'
 
 const myWork = [
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -13,20 +15,24 @@ const myWork = [
   },
   {
     'title': "Portfolio Boilerplate",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/example2.png",
       'comment': ""
     }
   },
-    {
-      'title': "Text to Speech WebApp on AWS",
-      'image': {
-        'desc': "example screenshot of a project using AWS",
-        'src': "images/example3.png",
-        'comment': ""
+  {
+    'title': "Text to Speech WebApp on AWS",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "example screenshot of a project using AWS",
+      'src': "images/example3.png",
+      'comment': ""
     }
   }
 ]
 
-ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'))
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
