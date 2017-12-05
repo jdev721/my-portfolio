a7b05c7822aa4edaff9ca08aadb8e9ca28e0420b
Jorge Rodriguez <captainjorge@gmail.com>
2017-12-04 21:22:13 -0500

    Added new images and descriptions for Work Examples

--- js/main.js
+++ js/main.js
@@ -4,32 +4,46 @@ import ExampleWork from './example-work'
 
 const myWork = [
   {
-    'title': "Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "A Serverless Portfolio",
+    'href': "images/PortfolioCode.jpg",
+    'desc': `When you visit this portfolio, you go to my domain managed by Amazon Route 53. This directs the request to the Cloudfront Content Delivery Network, which distributes my portfolio worldwide. Cloudfront retrieves the portfolio from an Amazon Simple Storage Service (S3) Bucket, the portfolio that arrives on your device is a combination of HTML, CSS, JavaScript and Images.
+The frontend component, which is running on your device, uses the React JavaScript Library to provide interactivity. It also uses the Babel JavaScript compiler to ensure that it runs in almost any browser.
+Taken together, all these things make up the frontend of this application.
+To get the portfolio to S3: First, my machine was set up as a development environment. The source code is stored on GitHub. When the code is pushed to GitHub, Amazon Web Services CodeBuild takes over automatically, runs unit tests with Jest, packages the code using NPM, Webpack, and Babel, and stores the results in S3. CodePipeline will then run a Lambda function that deploys the code to my S3 Bucket. The Lambda function also sends a message using AWS Simple Notification Service (SNS), to notify me that my portfolio has been deployed.
+`,
     'image': {
-      'desc': "example screenshot of a project involving code",
-      'src': "images/example1.png",
+      'desc': "Screenshot of Serverless Portfolio project code",
+      'src': "images/PortfolioCode.jpg",
       'comment': ""
     }
   },
   {
-    'title': "Portfolio Boilerplate",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "Note Converter Application",
+    'href': "images/example2.png",
+    'desc': `The website is hosted in S3. In this website you enter your notes. Once you've entered those notes in the site executes a post to
+an Amazon API Gateway, which triggers a Lambda function. This function puts the notes into Amazon DynamoDB, a managed noSQL Database. Lambda then triggers an SNS event that triggers another Lambda function, which takes the notes and passes them to the Amazon Polly Service, which in turn returns the notes as MP3 audio file.
+The MP3 file is saved to S3 bucket. The Lambda function then updates DynamoDB stating that the notes have been converted into audio.
+
+Then you visit the site and search for notes. This triggers API Gateway, which triggers a Lambda function to scan DynamoDB and return the notes.
+`,
     'image': {
-      'desc': "A Serverless Portfolio",
-      'src': "images/example2.png",
+      'desc': "Note Converter Application",
+      'src': "images/Polly.jpg",
       'comment': ""
     }
   },
   {
-    'title': "Text to Speech WebApp on AWS",
+    'title': "Alexa Skill",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': `In this project I programmed an Alexa Skill in order to be able to interact with and hear
+    the notes I have created using the Note to Speech project.
+    It uses AWS Lambda and the Amazon Developer platform to set things like invocation name, intent schema, slot type,
+    and utterances. The completed Skill reads MP3 files stored in S3 Buckets and selects them randomly - so Alexa is reading
+    out random facts and helping to practice learning notes.
+    `,
     'image': {
-      'desc': "example screenshot of a project using AWS",
-      'src': "images/example3.png",
+      'desc': "Screenshot of Alexa Skill",
+      'src': "images/AlexaSkill.jpg",
       'comment': ""
     }
   }
